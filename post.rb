fd3a776b9f9c7740d137467b1caa45b2af19d4ac
kartinich <dkostrikin@inbox.ru>
2020-04-03 19:10:05 +0300

    Version 1.0

---
+++ post.rb
@@ -0,0 +1,39 @@
+class Post
+
+  def self.post_types
+    [Memo, Task, Link]
+  end
+
+  def self.create(type_index)
+    post_types[type_index].new
+  end
+
+  def initialize
+    @created_at = Time.now
+    @text = []
+  end
+
+  def read_from_console
+    #todo
+  end
+
+  def to_strings
+    # todo
+  end
+
+  def save
+    file = File.new(file_path, 'w')
+
+    to_strings.each { |string| file.puts(string) }
+
+    file.close
+  end
+
+  def file_path
+    current_path = File.dirname(__FILE__)
+
+    file_time = @created_at.strftime('%Y-%m-%d_%H-%M-%S')
+
+    "#{current_path}/#{self.class.name}_#{file_time}.txt"
+  end
+end
